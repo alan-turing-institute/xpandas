41d4fca13e47fd19fdc37ea92501718051f99fc8
Vitaly Davydov <1061040@gmail.com>
2017-08-29 14:19:10 +0100

    Tests for Series transformer for MultiDataFrame

--- tests/test_transformer.py
+++ tests/test_transformer.py
@@ -9,77 +9,77 @@ import pandas as pd
 import numpy as np
 
 
-# def test_transformer_custom():
-#     s = MultiSeries([
-#         pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-#         pd.Series([4, 5, 6], index=['d', 'e', 'g'])
-#     ])
-#
-#     series_transformer = CustomTransformer(transform_function=lambda series: series.mean())
-#     series_transformer = series_transformer.fit()
-#
-#     s_transformed = series_transformer.transform(s)
-#
-#     assert type(s_transformed) == MultiSeries
-#     assert s_transformed.data_type == np.float64
-#
-#
-# def test_transformer_custom_to_data_frame():
-#     s = MultiSeries([
-#         pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-#         pd.Series([4, 5, 6], index=['d', 'e', 'g'])
-#     ])
-#
-#     series_transformer = CustomTransformer(transform_function=lambda series: {'mean': series.mean()})
-#     series_transformer = series_transformer.fit()
-#
-#     s_transformed = series_transformer.transform(s)
-#
-#     assert type(s_transformed) == MultiDataFrame
-#
-#
-# def test_transformer_custom_series_to_series():
-#     s = MultiSeries([
-#         pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-#         pd.Series([4, 5, 6], index=['d', 'e', 'g'])
-#     ])
-#
-#     series_transformer = CustomTransformer(transform_function=lambda series: series + 1)
-#     series_transformer = series_transformer.fit()
-#
-#     s_transformed = series_transformer.transform(s)
-#
-#     assert type(s_transformed) == MultiSeries
-#     assert s_transformed.data_type == pd.Series
-#
-#
-# def test_transformer_series_transformer():
-#     s = MultiSeries([
-#         pd.Series([1, 2, 3], index=['a', 'b', 'c']),
-#         pd.Series([4, 5, 6], index=['d', 'e', 'g'])
-#     ])
-#
-#     series_transformer = TimeSeriesTransformer()
-#     series_transformer = series_transformer.fit()
-#
-#     transformed_series = series_transformer.transform(s)
-#
-#     assert type(transformed_series) == MultiDataFrame
-#
-#
-# def test_transformer_series_to_series_transformer():
-#     s = MultiSeries([
-#         pd.Series(np.random.normal(0, 10, 100)),
-#         pd.Series(np.random.uniform(-100, 100, 150)),
-#         pd.Series(np.random.random_integers(0, 500, 200))
-#     ])
-#
-#     series_to_series_transformer = TimeSeriesWindowTransformer().fit()
-#     transformed_series = series_to_series_transformer.transform(s)
-#
-#     assert series_to_series_transformer.transform_func(s[0]).equals(transformed_series[0])
-#     assert transformed_series.data_type == pd.Series
-#     assert type(transformed_series) == MultiSeries
+def test_transformer_custom():
+    s = MultiSeries([
+        pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+        pd.Series([4, 5, 6], index=['d', 'e', 'g'])
+    ])
+
+    series_transformer = CustomTransformer(transform_function=lambda series: series.mean())
+    series_transformer = series_transformer.fit()
+
+    s_transformed = series_transformer.transform(s)
+
+    assert type(s_transformed) == MultiSeries
+    assert s_transformed.data_type == np.float64
+
+
+def test_transformer_custom_to_data_frame():
+    s = MultiSeries([
+        pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+        pd.Series([4, 5, 6], index=['d', 'e', 'g'])
+    ])
+
+    series_transformer = CustomTransformer(transform_function=lambda series: {'mean': series.mean()})
+    series_transformer = series_transformer.fit()
+
+    s_transformed = series_transformer.transform(s)
+
+    assert type(s_transformed) == MultiDataFrame
+
+
+def test_transformer_custom_series_to_series():
+    s = MultiSeries([
+        pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+        pd.Series([4, 5, 6], index=['d', 'e', 'g'])
+    ])
+
+    series_transformer = CustomTransformer(transform_function=lambda series: series + 1)
+    series_transformer = series_transformer.fit()
+
+    s_transformed = series_transformer.transform(s)
+
+    assert type(s_transformed) == MultiSeries
+    assert s_transformed.data_type == pd.Series
+
+
+def test_transformer_series_transformer():
+    s = MultiSeries([
+        pd.Series([1, 2, 3], index=['a', 'b', 'c']),
+        pd.Series([4, 5, 6], index=['d', 'e', 'g'])
+    ])
+
+    series_transformer = TimeSeriesTransformer()
+    series_transformer = series_transformer.fit()
+
+    transformed_series = series_transformer.transform(s)
+
+    assert type(transformed_series) == MultiDataFrame
+
+
+def test_transformer_series_to_series_transformer():
+    s = MultiSeries([
+        pd.Series(np.random.normal(0, 10, 100)),
+        pd.Series(np.random.uniform(-100, 100, 150)),
+        pd.Series(np.random.random_integers(0, 500, 200))
+    ])
+
+    series_to_series_transformer = TimeSeriesWindowTransformer().fit()
+    transformed_series = series_to_series_transformer.transform(s)
+
+    assert series_to_series_transformer.transform_func(s[0]).equals(transformed_series[0])
+    assert transformed_series.data_type == pd.Series
+    assert type(transformed_series) == MultiSeries
 
 
 def test_transformer_data_frame():
@@ -122,6 +122,4 @@ def test_transformer_data_frame():
     data_frame_transformer = TimeSeriesTransformer().fit()
     transformers_df = data_frame_transformer.transform(df)
 
-    print(
-        transformers_df
-    )
+    assert transformers_df.shape[1] == 6
