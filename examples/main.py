9d7b09e86098623973e6c937caeb53fb5a56d443
Vitaly Davydov <1061040@gmail.com>
2017-09-01 18:54:33 +0100

    Moved files

--- examples/main.py
+++ examples/main.py
@@ -1,3 +1,32 @@
-# TODO
-# http://timeseriesclassification.com/ for data sets
+from io import BytesIO
+from zipfile import ZipFile
+from urllib.request import urlopen
 
+import numpy as np
+import pandas as pd
+import os, sys
+import requests
+
+sys.path.insert(0, '..')
+
+from src.data_container import *
+from src.transformers.transformer import MeanSeriesTransformer, TimeSeriesWindowTransformer
+
+# Read a TimeSeries from URL
+url = urlopen("http://timeseriesclassification.com/Downloads/FordA.zip")
+zipfile = ZipFile(BytesIO(url.read()))
+lines = zipfile.open('FordA/FordA.csv').readlines()
+lines = [l.decode('utf-8') for l in lines]
+lines = lines[505:]
+# lines now is a list of strings with of timeseries in comma separeted format
+# 505 is a offset for the beginning of seriases
+
+lines = [list(map(float, l.split(','))) for l in lines]
+
+# now let's create a list of pd.Series
+lines = [pd.Series(l) for l in lines]
+
+# create a MultiSeries series of pd.Series objects
+X = MultiSeries(lines)
+
+windows_transformed_data =
