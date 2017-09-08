ff84520fd6682df31efeb2ae5a3c72937e48def5
Vitaly Davydov <1061040@gmail.com>
2017-09-08 15:22:38 +0100

    Series transformers structure

---
+++ transformers/transformers/series_transformers/__init__.py
@@ -0,0 +1,1 @@
+from .series_transformer import *
--- transformers/transformers/transformer.py
+++ transformers/transformers/transformer.py
@@ -1,7 +1,6 @@
 from ..data_container import MultiDataFrame, MultiSeries
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
-import tsfresh
 
 
 class CustomTransformer(BaseEstimator, TransformerMixin):
@@ -69,96 +68,6 @@ class CustomTransformer(BaseEstimator, TransformerMixin):
         # Because of self._check_input function
         return self._transform_data_frame(X, columns)
 
-
-class TimeSeriesTransformer(CustomTransformer):
-    FEATURES = [
-        'mean', 'std', 'max', 'min',
-        'median', 'quantile_25', 'quantile_75',
-        'quantile_90', 'quantile_95'
-    ]
-
-    def __init__(self, features=None, **kwargs):
-        accepted_types = [
-            pd.Series
-        ]
-
-        if features is None:
-            features = self.FEATURES
-        else:
-            for f in features:
-                if f not in self.FEATURES:
-                    raise ValueError('Unrecognized feature {}. Available features {}'.format(f, self.FEATURES))
-
-        def series_transform(series):
-            transformed_series = {}
-
-            for f in features:
-                if f.startswith('quantile_'):
-                    quant_rate = int(f.split('_')[1]) / 100.
-                    transformed_series[f] = series.quantile(quant_rate)
-                else:
-                    method_to_call = getattr(series, f)
-                    result = method_to_call()
-                    transformed_series[f] = result
-
-            return transformed_series
-
-        super(TimeSeriesTransformer, self).__init__(data_types=accepted_types,
-                                                    transform_function=series_transform)
-
-
-class TimeSeriesWindowTransformer(CustomTransformer):
-    def __init__(self, windows_size=3, **kwargs):
-        accepted_types = [
-            pd.Series
-        ]
-
-        def series_transform(series, **params):
-            return series.rolling(window=windows_size).mean()
-
-        super(TimeSeriesWindowTransformer, self).__init__(data_types=accepted_types,
-                                                          transform_function=series_transform)
-
-
-class MeanSeriesTransformer(CustomTransformer):
-    def __init__(self, **kwargs):
-        self.total_mean = None
-        accepted_types = [
-            pd.Series
-        ]
-
-        super(MeanSeriesTransformer, self).__init__(data_types=accepted_types)
-
-    def fit(self, X, **kwargs):
-        super(MeanSeriesTransformer, self).fit(X, **kwargs)
-        sum_and_size = X.apply(lambda s: (s.sum(), len(s)))
-        sum_total = sum([x[0] for x in sum_and_size])
-        total_size = sum([x[1] for x in sum_and_size])
-        self.total_mean = sum_total / total_size
-        return self
-
-    def transform(self, X, columns=None):
-        f = lambda s: self.total_mean - s.mean()
-        self.transform_function = f
-        return super(MeanSeriesTransformer, self).transform(X, columns)
-
-
-class TsFreshSeriesTransformer(CustomTransformer):
-    def __init__(self, **kwargs):
-        accepted_types = [
-            pd.Series
-        ]
-
-        def series_transform(series):
-            _df = pd.DataFrame({
-                'series': series,
-                'id': [1 for _ in range(len(series))]
-            })
-            return tsfresh.extract_features(_df, column_id='id')
-
-        super(TsFreshSeriesTransformer, self).__init__(data_types=accepted_types,
-                                                       columns=None,
-                                                       transform_function=series_transform)
 
 
 class PipeLineChain(TransformerMixin):
