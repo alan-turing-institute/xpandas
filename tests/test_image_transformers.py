ab07267edda4533805eab53b61cf15804a800c40
Vitaly Davydov <1061040@gmail.com>
2017-09-19 18:10:28 +0100

    Image transformer (simple wrapper over scimitar image) + tests

---
+++ tests/test_image_transformers.py
@@ -0,0 +1,36 @@
+import os
+import numpy as np
+
+from ..transformers.data_container import MultiSeries, MultiDataFrame
+from ..transformers.transformers.image_transformer import ImageTransformer
+import skimage.transform as skimage_transform
+
+n = 20
+m = 20
+colours_n = 255
+
+
+def generate_image(is_3d=True):
+    if is_3d:
+        return (np.random.rand(30, 30, 3) * 255).astype('uint8')
+    return (np.random.rand(30, 30) * 255).astype('uint8')
+
+
+def test_image_transformation():
+    s = MultiSeries([generate_image(False) for _ in range(100)])
+
+    try:
+        image_transformer = ImageTransformer().fit()
+        assert False
+    except:
+        assert True
+
+    image_transformer = ImageTransformer(skimage_transform.hough_circle, radius=5).fit()
+    s_transformed = image_transformer.transform(s)
+
+    assert s_transformed.data_type == np.ndarray
+
+    image_transformer = ImageTransformer(skimage_transform.resize, output_shape=(10, 10)).fit()
+    s_transformed = image_transformer.transform(s)
+
+    assert s_transformed.data_type == np.ndarray
---
+++ transformers/transformers/image_transformer/image_transformer.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+
+from ..transformer import CustomTransformer
+
+
+class ImageTransformer(CustomTransformer):
+    def __init__(self, skimage_function=None, **function_params):
+        accepted_types = [
+            list, np.ndarray, np.array
+        ]
+
+        if skimage_function is None:
+            raise Exception('Please specify transform function from scikit-image'
+                            ' http://scikit-image.org/docs/dev/api/skimage.transform.html')
+
+        def image_transform_function(img):
+            return skimage_function(img, **function_params)
+
+        super(ImageTransformer, self).__init__(data_types=accepted_types,
+                                               columns=None,
+                                               transform_function=image_transform_function)
