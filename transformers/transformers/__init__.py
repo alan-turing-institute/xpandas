b98f352a57d7a66d7d22c1a6c3d5b0dc43548687
Vitaly Davydov <1061040@gmail.com>
2017-09-21 16:11:42 +0100

    TsFreshSeriesTransformer example

--- transformers/transformers/__init__.py
+++ transformers/transformers/__init__.py
@@ -1,3 +1,3 @@
 from .pipeline_transformer import PipeLineChain
-from .series_transformers import TimeSeriesWindowTransformer, MeanSeriesTransformer, TimeSeriesTransformer
+from .series_transformers import TimeSeriesWindowTransformer, MeanSeriesTransformer, TimeSeriesTransformer, TsFreshSeriesTransformer
 from .transformer import CustomTransformer, DataFrameTransformer
