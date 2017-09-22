4ea8fb7867813628094a8a3b57d60b5b366964d2
Vitaly Davydov <1061040@gmail.com>
2017-09-22 16:42:31 +0100

    Setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='XPandas',
+      version='0.1',
+      description='1d/2d data container with map-reduce transformers',
+      url='https://github.com/kiraly-group/XPandas/tree/development',
+      author='Vitaly Davydov',
+      author_email='1061040@gmail.com',
+      license='BSD',
+      packages=['XPandas'],
+      zip_safe=False)
