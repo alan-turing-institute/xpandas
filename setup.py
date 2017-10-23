cc7793001ad70915e70d80e2211cbc62a1dd4b1a
Vitaly Davydov <1061040@gmail.com>
2017-10-23 17:56:12 +0100

    Changed deployment script

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
 
 setup(name='XPandas',
-      version='1.0',
+      version='1.0.2',
       description='1d/2d data container with map-reduce transformers',
       url='https://github.com/alan-turing-institute/xpandas',
       author='Vitaly Davydov (@iwitaly)',
       author_email='1061040@gmail.com',
       license='BSD',
       keywords='data container sklearn pandas map reduce transformer',
-      packages=['xpandas'],
+      packages=find_packages(),
       zip_safe=False)
